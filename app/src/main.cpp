dda914c4c0e23bc42bed34f9ca836f94dde36f5d
Dan Orban <dtorban@umn.edu>
2018-02-19 09:43:11 -0600

    Switched to multithreaded app.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,29 +2,75 @@
 
 #include "OpenGL.h"
 
-
-// MinVR header
-#include <main/VRMain.h>
+#include "VRMultithreadedApp.h"
 using namespace MinVR;
 
-// Just included for some simple Matrix math used below
-// This is not required for use of MinVR in general
-#include <math/VRMath.h>
 
+                // Create VBO
+                float vertices[]  = { 1.0f, 1.0f, 1.0f,  -1.0f, 1.0f, 1.0f,  -1.0f,-1.0f, 1.0f,      // v0-v1-v2 (front)
+                        -1.0f,-1.0f, 1.0f,   1.0f,-1.0f, 1.0f,   1.0f, 1.0f, 1.0f,      // v2-v3-v0
 
+                        1.0f, 1.0f, 1.0f,   1.0f,-1.0f, 1.0f,   1.0f,-1.0f,-1.0f,      // v0-v3-v4 (right)
+                        1.0f,-1.0f,-1.0f,   1.0f, 1.0f,-1.0f,   1.0f, 1.0f, 1.0f,      // v4-v5-v0
+
+                        1.0f, 1.0f, 1.0f,   1.0f, 1.0f,-1.0f,  -1.0f, 1.0f,-1.0f,      // v0-v5-v6 (top)
+                        -1.0f, 1.0f,-1.0f,  -1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,      // v6-v1-v0
+
+                        -1.0f, 1.0f, 1.0f,  -1.0f, 1.0f,-1.0f,  -1.0f,-1.0f,-1.0f,      // v1-v6-v7 (left)
+                        -1.0f,-1.0f,-1.0f,  -1.0f,-1.0f, 1.0f,  -1.0f, 1.0f, 1.0f,      // v7-v2-v1.0
+
+                        -1.0f,-1.0f,-1.0f,   1.0f,-1.0f,-1.0f,   1.0f,-1.0f, 1.0f,      // v7-v4-v3 (bottom)
+                        1.0f,-1.0f, 1.0f,  -1.0f,-1.0f, 1.0f,  -1.0f,-1.0f,-1.0f,      // v3-v2-v7
+
+                        1.0f,-1.0f,-1.0f,  -1.0f,-1.0f,-1.0f,  -1.0f, 1.0f,-1.0f,      // v4-v7-v6 (back)
+                        -1.0f, 1.0f,-1.0f,   1.0f, 1.0f,-1.0f,   1.0f,-1.0f,-1.0f };    // v6-v5-v4
+
+                // normal array
+                float normals[]   = { 0, 0, 1,   0, 0, 1,   0, 0, 1,      // v0-v1-v2 (front)
+                        0, 0, 1,   0, 0, 1,   0, 0, 1,      // v2-v3-v0
+
+                        1, 0, 0,   1, 0, 0,   1, 0, 0,      // v0-v3-v4 (right)
+                        1, 0, 0,   1, 0, 0,   1, 0, 0,      // v4-v5-v0
+
+                        0, 1, 0,   0, 1, 0,   0, 1, 0,      // v0-v5-v6 (top)
+                        0, 1, 0,   0, 1, 0,   0, 1, 0,      // v6-v1-v0
+
+                        -1, 0, 0,  -1, 0, 0,  -1, 0, 0,      // v1-v6-v7 (left)
+                        -1, 0, 0,  -1, 0, 0,  -1, 0, 0,      // v7-v2-v1
+
+                        0,-1, 0,   0,-1, 0,   0,-1, 0,      // v7-v4-v3 (bottom)
+                        0,-1, 0,   0,-1, 0,   0,-1, 0,      // v3-v2-v7
+
+                        0, 0,-1,   0, 0,-1,   0, 0,-1,      // v4-v7-v6 (back)
+                        0, 0,-1,   0, 0,-1,   0, 0,-1 };    // v6-v5-v4
+
+                // color array
+                float colors[]    = { 1, 1, 1,   1, 1, 0,   1, 0, 0,      // v0-v1-v2 (front)
+                        1, 0, 0,   1, 0, 1,   1, 1, 1,      // v2-v3-v0
+
+                        1, 1, 1,   1, 0, 1,   0, 0, 1,      // v0-v3-v4 (right)
+                        0, 0, 1,   0, 1, 1,   1, 1, 1,      // v4-v5-v0
+
+                        1, 1, 1,   0, 1, 1,   0, 1, 0,      // v0-v5-v6 (top)
+                        0, 1, 0,   1, 1, 0,   1, 1, 1,      // v6-v1-v0
+
+                        1, 1, 0,   0, 1, 0,   0, 0, 0,      // v1-v6-v7 (left)
+                        0, 0, 0,   1, 0, 0,   1, 1, 0,      // v7-v2-v1
+
+                        0, 0, 0,   0, 0, 1,   1, 0, 1,      // v7-v4-v3 (bottom)
+                        1, 0, 1,   1, 0, 0,   0, 0, 0,      // v3-v2-v7
+
+                        0, 0, 1,   0, 0, 0,   0, 1, 0,      // v4-v7-v6 (back)
+                        0, 1, 0,   0, 1, 1,   0, 0, 1 };    // v6-v5-v4
 
 /**
  * MyVRApp is an example of a modern OpenGL using VBOs, VAOs, and shaders.  MyVRApp inherits
  * from VRGraphicsApp, which allows you to override onVREvent to get input events, onRenderContext
  * to setup context sepecific objects, and onRenderScene that renders to each viewport.
  */
-class MyVRApp : public VREventHandler, VRRenderHandler {
+class MyVRApp : public VRMultithreadedApp {
 public:
-	MyVRApp(int argc, char** argv) {
-        vrMain = new VRMain();
-        vrMain->addEventHandler(this);
-        vrMain->addRenderHandler(this);
-        vrMain->initialize(argc,argv);
+	MyVRApp(int argc, char** argv) : VRMultithreadedApp(argc, argv) {
     }
 
 
@@ -47,89 +93,19 @@ public:
 
 		// Quit if the escape button is pressed
 		if (event.getName() == "KbdEsc_Down") {
-			vrMain->shutdown();
+			running = false;
 		}
 	}
 
-
-	/// onVRRenderContext is the override which allows users to setup context specific
-	/// variables like VBO's, VAO's, textures, framebuffers, and shader programs.
-    void onVRRenderContext(const VRDataIndex &stateData) {
-        if (stateData.exists("IsGraphics")) {
-            // If this is the inital call, initialize context variables
-            if ((int)stateData.getValue("InitRender") == 1) {
-                
-//#ifdef _WIN32
+    class Context : public VRAppSharedContext {
+    public:
+        Context(const MyVRApp& app, const VRGraphicsState &renderState) : app(app) {
                 glewExperimental = GL_TRUE;
                 GLenum err = glewInit();
                 if (GLEW_OK != err)
                 {
                     std::cout << "Error initializing GLEW." << std::endl;
                 }
-//#endif
-                
-                // Init GL
-                glEnable(GL_DEPTH_TEST);
-                glClearDepth(1.0f);
-                glDepthFunc(GL_LEQUAL);
-                glClearColor(0, 0, 0, 1);
-
-                // Create VBO
-                GLfloat vertices[]  = { 1.0f, 1.0f, 1.0f,  -1.0f, 1.0f, 1.0f,  -1.0f,-1.0f, 1.0f,      // v0-v1-v2 (front)
-                        -1.0f,-1.0f, 1.0f,   1.0f,-1.0f, 1.0f,   1.0f, 1.0f, 1.0f,      // v2-v3-v0
-
-                        1.0f, 1.0f, 1.0f,   1.0f,-1.0f, 1.0f,   1.0f,-1.0f,-1.0f,      // v0-v3-v4 (right)
-                        1.0f,-1.0f,-1.0f,   1.0f, 1.0f,-1.0f,   1.0f, 1.0f, 1.0f,      // v4-v5-v0
-
-                        1.0f, 1.0f, 1.0f,   1.0f, 1.0f,-1.0f,  -1.0f, 1.0f,-1.0f,      // v0-v5-v6 (top)
-                        -1.0f, 1.0f,-1.0f,  -1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f,      // v6-v1-v0
-
-                        -1.0f, 1.0f, 1.0f,  -1.0f, 1.0f,-1.0f,  -1.0f,-1.0f,-1.0f,      // v1-v6-v7 (left)
-                        -1.0f,-1.0f,-1.0f,  -1.0f,-1.0f, 1.0f,  -1.0f, 1.0f, 1.0f,      // v7-v2-v1.0
-
-                        -1.0f,-1.0f,-1.0f,   1.0f,-1.0f,-1.0f,   1.0f,-1.0f, 1.0f,      // v7-v4-v3 (bottom)
-                        1.0f,-1.0f, 1.0f,  -1.0f,-1.0f, 1.0f,  -1.0f,-1.0f,-1.0f,      // v3-v2-v7
-
-                        1.0f,-1.0f,-1.0f,  -1.0f,-1.0f,-1.0f,  -1.0f, 1.0f,-1.0f,      // v4-v7-v6 (back)
-                        -1.0f, 1.0f,-1.0f,   1.0f, 1.0f,-1.0f,   1.0f,-1.0f,-1.0f };    // v6-v5-v4
-
-                // normal array
-                GLfloat normals[]   = { 0, 0, 1,   0, 0, 1,   0, 0, 1,      // v0-v1-v2 (front)
-                        0, 0, 1,   0, 0, 1,   0, 0, 1,      // v2-v3-v0
-
-                        1, 0, 0,   1, 0, 0,   1, 0, 0,      // v0-v3-v4 (right)
-                        1, 0, 0,   1, 0, 0,   1, 0, 0,      // v4-v5-v0
-
-                        0, 1, 0,   0, 1, 0,   0, 1, 0,      // v0-v5-v6 (top)
-                        0, 1, 0,   0, 1, 0,   0, 1, 0,      // v6-v1-v0
-
-                        -1, 0, 0,  -1, 0, 0,  -1, 0, 0,      // v1-v6-v7 (left)
-                        -1, 0, 0,  -1, 0, 0,  -1, 0, 0,      // v7-v2-v1
-
-                        0,-1, 0,   0,-1, 0,   0,-1, 0,      // v7-v4-v3 (bottom)
-                        0,-1, 0,   0,-1, 0,   0,-1, 0,      // v3-v2-v7
-
-                        0, 0,-1,   0, 0,-1,   0, 0,-1,      // v4-v7-v6 (back)
-                        0, 0,-1,   0, 0,-1,   0, 0,-1 };    // v6-v5-v4
-
-                // color array
-                GLfloat colors[]    = { 1, 1, 1,   1, 1, 0,   1, 0, 0,      // v0-v1-v2 (front)
-                        1, 0, 0,   1, 0, 1,   1, 1, 1,      // v2-v3-v0
-
-                        1, 1, 1,   1, 0, 1,   0, 0, 1,      // v0-v3-v4 (right)
-                        0, 0, 1,   0, 1, 1,   1, 1, 1,      // v4-v5-v0
-
-                        1, 1, 1,   0, 1, 1,   0, 1, 0,      // v0-v5-v6 (top)
-                        0, 1, 0,   1, 1, 0,   1, 1, 1,      // v6-v1-v0
-
-                        1, 1, 0,   0, 1, 0,   0, 0, 0,      // v1-v6-v7 (left)
-                        0, 0, 0,   1, 0, 0,   1, 1, 0,      // v7-v2-v1
-
-                        0, 0, 0,   0, 0, 1,   1, 0, 1,      // v7-v4-v3 (bottom)
-                        1, 0, 1,   1, 0, 0,   0, 0, 0,      // v3-v2-v7
-
-                        0, 0, 1,   0, 0, 0,   0, 1, 0,      // v4-v7-v6 (back)
-                        0, 1, 0,   0, 1, 1,   0, 0, 1 };    // v6-v5-v4
 
                 // Allocate space and send Vertex Data
                 glGenBuffers(1, &vbo);
@@ -138,11 +114,35 @@ public:
                 glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
                 glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(normals), normals);
                 glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices)+sizeof(normals), sizeof(colors), colors);
+        }
+
+        ~Context() {
+                glDeleteBuffers(1, &vbo);
+        }
+
+        void update(const VRGraphicsState &renderState) {
+        }
+
+        GLuint getVbo() { return vbo; }
+
+    private:
+        const MyVRApp& app;
+        GLuint vbo;
+    };
+
+    class Renderer : public VRAppRenderer {
+    public:
+        Renderer(const MyVRApp& app, Context& sharedContext, const VRGraphicsState &renderState) : app(app), sharedContext(sharedContext) {   
+                // Init GL
+                glEnable(GL_DEPTH_TEST);
+                glClearDepth(1.0f);
+                glDepthFunc(GL_LEQUAL);
+                glClearColor(0, 0, 0, 1);
 
                 // Create vao
                 glGenVertexArrays(1, &vao);
                 glBindVertexArray(vao);
-                glBindBuffer(GL_ARRAY_BUFFER, vbo);
+                glBindBuffer(GL_ARRAY_BUFFER, sharedContext.getVbo());
                 glEnableVertexAttribArray(0);
                 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (char*)0);
                 glEnableVertexAttribArray(1);
@@ -165,8 +165,8 @@ public:
                         "out vec3 col;"
                         ""
                         "void main() { "
-                        "	gl_Position = ProjectionMatrix*ViewMatrix*ModelMatrix*vec4(position, 1.0); "
-                        "	col = color;"
+                        "   gl_Position = ProjectionMatrix*ViewMatrix*ModelMatrix*vec4(position, 1.0); "
+                        "   col = color;"
                         "}";
                 vshader = compileShader(vertexShader, GL_VERTEX_SHADER);
 
@@ -176,7 +176,7 @@ public:
                         "out vec4 colorOut;"
                         ""
                         "void main() { "
-                        "	colorOut = vec4(col, 1.0); "
+                        "   colorOut = vec4(col, 1.0); "
                         "}";
                 fshader = compileShader(fragmentShader, GL_FRAGMENT_SHADER);
 
@@ -185,33 +185,23 @@ public:
                 glAttachShader(shaderProgram, vshader);
                 glAttachShader(shaderProgram, fshader);
                 linkShaderProgram(shaderProgram);
-            }
+        }
 
-            // Destroy context items if the program is no longer running
-            /*
-
-             DAN K. to DAN O.:  I don't understand the isRunning(), why would
-             onRender() be called if the app is not running?
-
-            if (!isRunning()) {
-                glDeleteBuffers(1, &vbo);
+        virtual ~Renderer() {
                 glDeleteVertexArrays(1, &vao);
                 glDetachShader(shaderProgram, vshader);
                 glDetachShader(shaderProgram, fshader);
                 glDeleteShader(vshader);
                 glDeleteShader(fshader);
                 glDeleteProgram(shaderProgram);
-                return;
-            }
-            */
         }
-	}
 
-	/// onVRRenderScene will run draw calls on each viewport inside a context.
-    void onVRRenderScene(const VRDataIndex &stateData) {
-        if (stateData.exists("IsGraphics")) {
-            VRFloatArray projMat = stateData.getValue("ProjectionMatrix");
-            VRFloatArray viewMat = stateData.getValue("ViewMatrix");
+        void update(const MinVR::VRGraphicsState &renderState) {
+        }
+
+        void render(const MinVR::VRGraphicsState &renderState) {
+            const float* projMat = renderState.getProjectionMatrix();
+            const float* viewMat = renderState.getViewMatrix();
 
             // clear screen
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
@@ -219,11 +209,11 @@ public:
             // Set shader parameters
             glUseProgram(shaderProgram);
             GLint loc = glGetUniformLocation(shaderProgram, "ProjectionMatrix");
-            glUniformMatrix4fv(loc, 1, GL_FALSE, &projMat[0]);
+            glUniformMatrix4fv(loc, 1, GL_FALSE, projMat);
             loc = glGetUniformLocation(shaderProgram, "ViewMatrix");
-            glUniformMatrix4fv(loc, 1, GL_FALSE, &viewMat[0]);
+            glUniformMatrix4fv(loc, 1, GL_FALSE, viewMat);
             loc = glGetUniformLocation(shaderProgram, "ModelMatrix");
-            glUniformMatrix4fv(loc, 1, GL_FALSE, model);
+            glUniformMatrix4fv(loc, 1, GL_FALSE, app.model);
 
             // Draw cube
             glBindVertexArray(vao);
@@ -233,48 +223,56 @@ public:
             // reset program
             glUseProgram(0);
         }
-	}
 
-	/// Compiles shader
-	GLuint compileShader(const std::string& shaderText, GLuint shaderType) {
-		const char* source = shaderText.c_str();
-		int length = (int)shaderText.size();
-		GLuint shader = glCreateShader(shaderType);
-		glShaderSource(shader, 1, &source, &length);
-		glCompileShader(shader);
-		GLint status;
-		glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-		if(status == GL_FALSE) {
-			GLint length;
-			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
-			std::vector<char> log(length);
-			glGetShaderInfoLog(shader, length, &length, &log[0]);
-			std::cerr << &log[0];
-		}
+        /// Compiles shader
+        GLuint compileShader(const std::string& shaderText, GLuint shaderType) {
+            const char* source = shaderText.c_str();
+            int length = (int)shaderText.size();
+            GLuint shader = glCreateShader(shaderType);
+            glShaderSource(shader, 1, &source, &length);
+            glCompileShader(shader);
+            GLint status;
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+            if(status == GL_FALSE) {
+                GLint length;
+                glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+                std::vector<char> log(length);
+                glGetShaderInfoLog(shader, length, &length, &log[0]);
+                std::cerr << &log[0];
+            }
 
-		return shader;
-	}
+            return shader;
+        }
 
-	/// links shader program
-	void linkShaderProgram(GLuint shaderProgram) {
-		glLinkProgram(shaderProgram);
-		GLint status;
-		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
-		if(status == GL_FALSE) {
-			GLint length;
-			glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &length);
-			std::vector<char> log(length);
-			glGetProgramInfoLog(shaderProgram, length, &length, &log[0]);
-			std::cerr << "Error compiling program: " << &log[0] << std::endl;
-		}
-	}
+        /// links shader program
+        void linkShaderProgram(GLuint shaderProgram) {
+            glLinkProgram(shaderProgram);
+            GLint status;
+            glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+            if(status == GL_FALSE) {
+                GLint length;
+                glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &length);
+                std::vector<char> log(length);
+                glGetProgramInfoLog(shaderProgram, length, &length, &log[0]);
+                std::cerr << "Error compiling program: " << &log[0] << std::endl;
+            }
+        }
 
-    void run() {
-        while (vrMain->mainloop()) {}
+    private:
+        const MyVRApp& app;
+        GLuint vao, vshader, fshader, shaderProgram;
+        const Context& sharedContext;
+    };
+
+    VRAppSharedContext* createSharedContext(const VRGraphicsState &renderState) {
+        return new Context(*this, renderState);
+    }
+
+    VRAppRenderer* createRenderer(VRAppSharedContext& sharedContext, const VRGraphicsState &renderState) {
+        return new Renderer(*this, *static_cast<Context*>(&sharedContext), renderState);
     }
 
 private:
-	GLuint vbo, vao, vshader, fshader, shaderProgram;
 	float model[16];
     VRMain *vrMain;
 };
