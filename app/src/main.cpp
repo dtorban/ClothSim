08db9874275e250eaaf7eb7c8b05bf26240e9a29
Dan Orban <dtorban@umn.edu>
2018-02-21 16:57:12 -0600

    Added normal calculation.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -128,6 +128,34 @@ public:
             simTime += dt;
             count++;
         }
+
+        normals = calculateNormals(indices, cloth.getPositions());
+    }
+
+    std::vector<glm::vec3> calculateNormals(const std::vector<unsigned int>& inds, const std::vector<glm::vec3>& positions) {
+        std::vector<glm::vec3> norms(positions.size());
+        std::vector<int> numNorms(positions.size());
+
+        for (int f = 0; f < indices.size(); f+=3) {
+            const glm::vec3& a = positions[inds[f]];
+            const glm::vec3& b = positions[inds[f+1]];
+            const glm::vec3& c = positions[inds[f+2]];
+            glm::vec3 n = glm::normalize(glm::cross(b-a, c-a));
+            norms[inds[f]] += n;
+            norms[inds[f+1]] += n;
+            norms[inds[f+2]] += n;
+            numNorms[inds[f]]++;
+            numNorms[inds[f+1]]++;
+            numNorms[inds[f+2]]++;
+        }
+
+        for (int f = 0; f < norms.size(); f++) {
+            if (numNorms[f] > 0) {
+                norms[f] = norms[f]/(float(numNorms[f]));
+            }
+        }
+
+        return norms;
     }
 
     class Context : public VRAppSharedContext {
@@ -207,20 +235,23 @@ public:
                         "uniform mat4 NormalMatrix; "
                         ""
                         "out vec3 col;"
+                        "out vec3 norm;"
                         ""
                         "void main() { "
                         "   gl_Position = ProjectionMatrix*ViewMatrix*ModelMatrix*vec4(position, 1.0); "
                         "   col = color;"
+                        "   norm = normal;"
                         "}";
                 vshader = compileShader(vertexShader, GL_VERTEX_SHADER);
 
                 std::string fragmentShader =
                         "#version 330 \n"
                         "in vec3 col;"
+                        "in vec3 norm;"
                         "out vec4 colorOut;"
                         ""
                         "void main() { "
-                        "   colorOut = vec4(col, 1.0); "
+                        "   colorOut = vec4((norm/2.0) + 0.5, 1.0);"
                         "}";
                 fshader = compileShader(fragmentShader, GL_FRAGMENT_SHADER);
 
